75f457ddd77b563e4bf2357d5f610d6ce6fd6137
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-01 10:07:15 -0700

    test(): configure karma with sauce labs

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,47 @@
 module.exports = function(config) {
+  process.env.SAUCE_USERNAME='angular-ci';
+  process.env.SAUCE_ACCESS_KEY='9b988f434ff8-fbca-8aa4-4ae3-35442987';
+
+  var customLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7'
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '27'
+    },
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.9',
+      version: '7.1'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }
+  };
+
   config.set({
     frameworks: ['jasmine'],
     files: [
-      'node_modules/harmony-reflect/reflect.js',
       'dom-interceptor.js',
       'dom-interceptor_test.js'
     ],
     exclude: [],
     preprocessors: {},
-    browsers: ['Chrome_with_ECMA6'],
-    customLaunchers: {
-      Chrome_with_ECMA6: {
-        base: 'Chrome',
-        flags: ['--js-flags=--harmony']
-      }
-    }
+    SauceLabs: {
+      testName: 'Dom Interceptor Unit Tests',
+      recordScreenshots: false
+    },
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+    reporters: ['dots', 'saucelabs'],
+    singleRun: true
   });
 };
